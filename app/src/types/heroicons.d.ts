b05bcb5fa131fe348c1b0766467e06d6d00df6dc
Sandro Thornton <sandro.thornton@gmail.com>
2026-01-18 21:52:49 -0500

    Delete app directory

--- app/src/types/heroicons.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@heroicons/react/outline' {
-  import { FC, SVGProps } from 'react';
-  
-  export const ExclamationIcon: FC<SVGProps<SVGSVGElement>>;
-  // Add other icons as needed
-}
